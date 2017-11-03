8830d2cbfc32245abc30f3a37cd47912e9ee05e2
aabydava <david.aaby@northwestern.edu>
2017-11-03 16:56:27 -0500

    Edit 3
    added code to solve for odds ratio (OR) and sample size (N)

--- power_logistic.R
+++ power_logistic.R
@@ -29,21 +29,6 @@ LogisticSampleSize <- function(alpha=NULL, power=NULL, P0=NULL, OR=NULL, R=NULL,
   # N = sample size
 
   
-  ##################
-  # error messages #
-  ##################
-  if(any(alpha < 0 | alpha > 1)) stop('alpha not between 0 and 1')
-  if(any(power < 0 | power > 1)) stop('power not between 0 and 1')
-  if(any(P0 < 0 | P0 > 1))       stop('P0 not between 0 and 1')
-  if(any(R < 0 | R > 1))         stop('R not between 0 and 1')
-  if(any(OR < 0))                stop('OR not a positive value')
-  if(any(N < 2))                stop('N is less than 2')
-  
-  mylist = list(alpha, power, P0, OR, R, N)
-  l.mylist = lengths(mylist)
-  if(length(l.mylist[l.mylist>=2]) > 1) stop('Only vary one parameter at a time')
-
-  
   ############################
   # sample size calculations #
   ############################
@@ -103,6 +88,96 @@ LogisticSampleSize <- function(alpha=NULL, power=NULL, P0=NULL, OR=NULL, R=NULL,
     results = NULL
     
     if(length(alpha) > 1 | length(P0) > 1 | length(OR) > 1 | length(R) > 1 | length(N) > 1) {
+      rownum = max(length(alpha), length(power), length(P0), length(OR), length(R))
+      for(i in 1:rownum) {
+        results = cbind(alpha, power, P0, P1, OR, R, N)
+      }
+    }
+    
+    else {
+      results = c(alpha, power, P0, P1, OR, R, N)
+      results = data.frame(results)
+      results = t(results)
+      colnames(results) = c("alpha", "power", "P0", "P1", "OR", "R" ,"N")
+    }
+  }
+  
+  
+  # solve for odds ratio #
+  if(is.null(OR)) {
+    
+    beta = 1 - power
+    P1 = seq(0, .90, .00001)
+    N1 = N
+    OR = NULL
+    P1.new = NULL
+    
+    for(i in 1:length(P0)) {
+      Pbar = (1-R)*P0[i] + R*P1
+      n = (((qnorm(1-alpha/2)* sqrt(Pbar*(1-Pbar)/R)) + 
+              (qnorm(1-beta)*sqrt(P0[i]*(1-P0[i]) + ((P1*(1-P1)*(1-R))/R))))^2) / 
+        (((P0[i]-P1)^2) * (1-R))
+      
+      or = (P1*(1-P0[i])) / (P0[i]*(1-P1))
+      N.N1 = abs(n - N1)
+      foo = cbind(P1,N,or, N.N1)
+      foo = foo[which(foo[,3] >= 1),]
+      x = foo[which.min(foo[,4]),]
+      
+      or = x[[3]]
+      OR = c(OR, or)
+      OR = round(OR,3)
+      
+      p1 = x[[1]]
+      P1.new = c(P1.new, p1)
+      P1.new = round(P1.new,3)
+    }
+    
+    # output results #
+    results = NULL
+    
+    if(length(alpha) > 1 | length(power) > 1 | length(P0) > 1 | length(R) > 1 | length(N) > 1) {
+      rownum = max(length(alpha), length(power), length(P0), length(OR), length(R))
+      for(i in 1:rownum) {
+        results = cbind(alpha, power, P0, P1.new, OR, R, N)
+      }
+      colnames(results) = c("alpha", "power", "P0", "P1", "OR", "R" ,"N")
+    }
+    
+    else {
+      results = c(alpha, power, P0, P1.new, OR, R, N)
+      results = data.frame(results)
+      results = t(results)
+      colnames(results) = c("alpha", "power", "P0", "P1", "OR", "R" ,"N")
+    }
+  }
+  
+  
+  # solve for sample size (N) #
+  if(is.null(N)) {
+    
+    beta = 1 - power
+    
+    # first, solve for P1 #
+    #OR = P1(1-P0) / P0(1-P1)
+    P1 = (P0*OR) / (OR*P0 + 1 - P0)
+    
+    #calculate Pbar #
+    Pbar = (1-R)*P0 + R*P1
+    
+    A1 = qnorm(1-alpha/2)* sqrt(Pbar*(1-Pbar)/R)
+    A2 = qnorm(1-beta)*sqrt(P0*(1-P0) + ((P1*(1-P1)*(1-R))/R))
+    A3 = ((P0-P1)^2) * (1-R)
+    
+    N = ((A1 + A2)^2) / A3
+    N = round(N)
+    
+    P1 = round(P1,3)
+    
+    # output results #
+    results = NULL
+    
+    if(length(alpha) > 1 | length(power) > 1 | length(P0) > 1 | length(OR) > 1 | length(R) > 1) {
       rownum = max(length(alpha), length(power), length(P0), length(OR), length(R))
       for(i in 1:rownum) {
         results = cbind(alpha, power, P0, P1, OR, R, N)
